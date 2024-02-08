5c4f146735bb2c27413e97ac07893685d56a1d8d
hyerim <hyerim>
2024-02-08 17:05:16 +0900

    Feater : qr fix

--- qrcodeChange/views.py
+++ qrcodeChange/views.py
@@ -6,6 +6,7 @@ from django.http import FileResponse,HttpResponse
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from PIL import Image
+from pathlib import Path
 import qrcode
 
 from io import BytesIO
@@ -25,7 +26,9 @@ def generate_qrcode(request):
         print(file_name) #이름뽑아오기
         #이미지 임시저장 테스트
         img = Image.open(file)
-        img.save('D:/kioskImage/'+file_name) 
+        current_path = Path(__file__).parent
+        img_path = current_path / 'kioskImg' / file_name
+        img.save(img_path) 
         #s3클라이언트 생성
         s3 = boto3.client(
                 service_name='s3',
@@ -34,14 +37,17 @@ def generate_qrcode(request):
                 aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
             )
         try: 
-            s3.upload_file('D:/kioskImage/'+file_name,"kioskphotoimage",file_name)
+            s3.upload_file(img_path,"kioskphotoimage",file_name)
         except Exception as e : print(e)
         imagesUrl = 'https://kioskphotoimage.s3.ap-northeast-2.amazonaws.com/'+file_name
         images = qrcode.make(imagesUrl)
-        images.save('D:/kioskImage/qr.jpg')
+        img_path = current_path / 'kioskImg'/'QR.jpg'
+        images.save(img_path)
+        #이미지 합성하기 코드 *짜야함*
 
         byte_io = BytesIO()
-        images.save(byte_io, 'PNG')
+        # images.save(byte_io, 'PNG')
         byte_io = byte_io.getvalue()
+        #이미지 리턴해주기
         return HttpResponse(byte_io, content_type='image/png')
         # return FileResponse(byte_io, as_attachment=True, filename='qrcode.png')
